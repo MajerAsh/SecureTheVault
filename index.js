877a804d4444cb3051be075c89bfe5c266db626e
Ashley Majer <Majerash@gmail.com>
2025-06-11 18:53:38 -0400

    secure the vault

--- index.js
+++ index.js
@@ -5,6 +5,4 @@ const code3 = (78 % 5) + 36;
 const message = "The vault has been secured. The combination is:";
 const codeA = code1 + "-" + code2 + "-" + code3;
 const codeB = `${code1}-${code2}-${code3}`;
-console.log(message);
-console.log(codeA);
-console.log(codeB);
+console.log(message, codeA, codeB);
