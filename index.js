506ac04cba0c7b0dc448972e3a25bac2882b11cb
Ashley Majer <Majerash@gmail.com>
2025-06-11 13:43:17 -0400

    vault

---
+++ index.js
@@ -0,0 +1,11 @@
+let code1;
+code1 = 7 + 3;
+
+let code2 = 200 / 4 - 10;
+
+let code3 = 80 + (90 % 2);
+
+let message = "The vault has been secured. the combo is:";
+let codeA = code1 + "-" + code2 + "-" + code3;
+const codeB = `${code1}-${code2}-${code3}`;
+console.log(message, codeA, codeB);
