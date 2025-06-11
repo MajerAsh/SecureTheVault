84bf88e5778657ff2525db370c8c76b113433b21
Ashley Majer <Majerash@gmail.com>
2025-06-11 18:43:13 -0400

    vault

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 let code1;
 code1 = 7 + 3;
-
-let code2 = 200 / 4 - 10;
-
-let code3 = 80 + (90 % 2);
-
-let message = "The vault has been secured. the combo is:";
-let codeA = code1 + "-" + code2 + "-" + code3;
+const code2 = 84 / 2 - 2;
+const code3 = (78 % 5) + 36;
+const message = "The vault has been secured. The combination is:";
+const codeA = code1 + "-" + code2 + "-" + code3;
 const codeB = `${code1}-${code2}-${code3}`;
-console.log(message, codeA, codeB);
+console.log(message);
+console.log(codeA);
+console.log(codeB);
